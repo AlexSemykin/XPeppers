a63914a3a4bbcc55e419bd3bfd24dbe00d1fcfda
Alex Semykin <oleksandr.semykin@xpeppers.com>
2019-03-01 15:55:54 +0100

    [p.128] Added SplitTemporaryVariable method.

---
+++ Refactoring/test/SplitTemporaryVariable/SplitTemporaryVariable.java
@@ -0,0 +1,12 @@
+package SplitTemporaryVariable;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SplitTemporaryVariable {
+    @Test
+    public void name() {
+
+    }
+}
