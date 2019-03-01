3e18fcb8fb8d0bade36787eb026316664490d3f1
Alex Semykin <oleksandr.semykin@xpeppers.com>
2019-03-01 16:19:26 +0100

    [p.128] Changed back the original variable to 'result'

--- Refactoring/src/SplitTemporaryVariable/SplitTemporaryVariable.java
+++ Refactoring/src/SplitTemporaryVariable/SplitTemporaryVariable.java
@@ -8,18 +8,16 @@ public class SplitTemporaryVariable {
     double _secondaryForce;
 
     double getDistanceTravelled(int time) {
-
+        double result;
         double acc = _primaryForce / _mass;
         int primaryTime = Math.min(time, _delay);
-        final double distance = 0.5 * acc * primaryTime * primaryTime;
+        result = 0.5 * acc * primaryTime * primaryTime;
         int secondaryTime = time - _delay;
-
-        double secondaryDistance = 0;
         if (secondaryTime > 0) {
             double primaryVel = acc * _delay;
             acc = (_primaryForce + _secondaryForce) / _mass;
-            secondaryDistance = primaryVel * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
+            result += primaryVel * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
         }
-        return distance + secondaryDistance;
+        return result;
     }
 }
