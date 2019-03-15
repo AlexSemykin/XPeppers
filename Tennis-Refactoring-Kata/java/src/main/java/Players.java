29eda88afc7cbc67b7a9531d1a3a3d60c9fefa24
Alex Semykin <alexsemykin21@yandex.ru>
2019-03-15 11:59:32 +0100

    Adds players Map to store player name and score.

--- Tennis-Refactoring-Kata/java/src/main/java/Players.java
+++ Tennis-Refactoring-Kata/java/src/main/java/Players.java
@@ -1,8 +1,22 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class Players {
-    private String nameFirstPlayer;
-    private String nameSecondPlayer;
+    private String firstPlayerName;
+    private String secondPlayerName;
 
     private Map<String, Integer> players;
+
+    public Players(String firstPlayerName, String secondPlayerName) {
+        this.firstPlayerName = firstPlayerName;
+        this.secondPlayerName = secondPlayerName;
+
+        initPlayers(firstPlayerName, secondPlayerName);
+    }
+
+    private void initPlayers(String firstPlayerName, String secondPlayerName) {
+        players = new HashMap<String, Integer>();
+        players.put(firstPlayerName, 0);
+        players.put(secondPlayerName, 0);
+    }
 }
