1bab546d46d03a263c58e063672196e4cce90918
Alex Semykin <alexsemykin21@yandex.ru>
2019-03-15 12:20:27 +0100

    Adds getters and setters to Player.

--- Tennis-Refactoring-Kata/java/src/main/java/Players.java
+++ Tennis-Refactoring-Kata/java/src/main/java/Players.java
@@ -2,21 +2,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Players {
-    private String firstPlayerName;
-    private String secondPlayerName;
+    private String player1Name;
+    private String player2Name;
 
     private Map<String, Integer> players;
 
-    public Players(String firstPlayerName, String secondPlayerName) {
-        this.firstPlayerName = firstPlayerName;
-        this.secondPlayerName = secondPlayerName;
+    public Players(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
 
-        initPlayers(firstPlayerName, secondPlayerName);
+        initPlayers(player1Name, player2Name);
     }
 
-    private void initPlayers(String firstPlayerName, String secondPlayerName) {
+    private void initPlayers(String player1Name, String player2Name) {
         players = new HashMap<String, Integer>();
-        players.put(firstPlayerName, 0);
-        players.put(secondPlayerName, 0);
+        players.put(player1Name, 0);
+        players.put(player2Name, 0);
     }
+
+    public String getPlayer1Name() {
+        return player1Name;
+    }
+
+    public String getPlayer2Name() {
+        return player2Name;
+    }
+
+    public void setPlayer1Score(int score){
+        players.put(getPlayer1Name(), score);
+    }
+
+    public void setPlayer2Score(int score){
+        players.put(getPlayer2Name(), score);
+    }
+
+
 }
--- Tennis-Refactoring-Kata/java/src/main/java/TennisGame1.java
+++ Tennis-Refactoring-Kata/java/src/main/java/TennisGame1.java
@@ -6,9 +6,13 @@ public class TennisGame1 implements TennisGame {
     private String player1Name;
     private String player2Name;
 
+    Players players;
+
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+
+        players = new Players(player1Name, player2Name);
     }
 
     public void wonPoint(String playerName) {
