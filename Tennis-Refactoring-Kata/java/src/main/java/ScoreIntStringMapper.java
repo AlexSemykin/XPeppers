735def4e24e79265cd3b0a694bd9dfb8c35c1e09
Alex Semykin <alexsemykin21@yandex.ru>
2019-03-18 12:13:54 +0100

    Inits scoreMapper and equalScoreMapper.

--- Tennis-Refactoring-Kata/java/src/main/java/ScoreIntStringMapper.java
+++ Tennis-Refactoring-Kata/java/src/main/java/ScoreIntStringMapper.java
@@ -8,5 +8,15 @@ public class ScoreIntStringMapper {
     public ScoreIntStringMapper(){
         scoreMapper = new HashMap<Integer, String>();
         equalScoreMapper = new HashMap<Integer, String>();
+
+        scoreMapper.put(0, "Love");
+        scoreMapper.put(1, "Fifteen");
+        scoreMapper.put(2, "Thirty");
+        scoreMapper.put(3, "Forty");
+
+        equalScoreMapper.put(0, "Love-All");
+        equalScoreMapper.put(1, "Fifteen-All");
+        equalScoreMapper.put(2, "Thirty-All");
+        equalScoreMapper.put(3, "Deuce-All");
     }
 }
