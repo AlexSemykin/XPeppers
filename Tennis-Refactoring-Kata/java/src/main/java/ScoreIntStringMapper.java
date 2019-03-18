17ab9de3e8ddfb953e31fd9365c6abfcdf6bd358
Alex Semykin <alexsemykin21@yandex.ru>
2019-03-18 12:17:43 +0100

    Adds getStringScore.

--- Tennis-Refactoring-Kata/java/src/main/java/ScoreIntStringMapper.java
+++ Tennis-Refactoring-Kata/java/src/main/java/ScoreIntStringMapper.java
@@ -6,17 +6,30 @@ public class ScoreIntStringMapper {
     private Map<Integer,String> equalScoreMapper;
 
     public ScoreIntStringMapper(){
+        initScoreMapper();
+        initEqualScoreMapper();
+    }
+
+    private void initScoreMapper() {
         scoreMapper = new HashMap<Integer, String>();
-        equalScoreMapper = new HashMap<Integer, String>();
 
         scoreMapper.put(0, "Love");
         scoreMapper.put(1, "Fifteen");
         scoreMapper.put(2, "Thirty");
         scoreMapper.put(3, "Forty");
+    }
+
+    private void initEqualScoreMapper() {
+        equalScoreMapper = new HashMap<Integer, String>();
 
         equalScoreMapper.put(0, "Love-All");
         equalScoreMapper.put(1, "Fifteen-All");
         equalScoreMapper.put(2, "Thirty-All");
         equalScoreMapper.put(3, "Deuce-All");
     }
+
+    public String getStringScore(int score){
+        return scoreMapper.get(score);
+    }
+
 }
